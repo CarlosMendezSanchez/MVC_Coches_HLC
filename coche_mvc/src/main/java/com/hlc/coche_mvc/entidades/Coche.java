f57524bcc2476191794f7b1923e451cd7eb31b65
Carlos <mendez.sanchez.carlos@iescamas.es>
2025-01-09 09:18:01 +0100

    coche anadido cambios

---
+++ coche_mvc/src/main/java/com/hlc/coche_mvc/entidades/Coche.java
@@ -0,0 +1,45 @@
+package com.hlc.coche_mvc.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * 
+ */
+@Entity
+public class Coche {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String marca;
+	private String color;
+	private String matricula;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+}
